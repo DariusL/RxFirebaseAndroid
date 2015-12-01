d5a0a03935a4ba25c05488d342bc2af54ecb1822
Darius Lapūnas <d.lapunas@gmail.com>
2015-12-01 15:36:21 +0200

    Add random test to try out drone

---
+++ rxfirebaseandroid/src/test/java/lt/dariusl/rxfirebaseandroid/TestTest.java
@@ -0,0 +1,12 @@
+package lt.dariusl.rxfirebaseandroid;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTest {
+    @Test
+    public void testTest() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
